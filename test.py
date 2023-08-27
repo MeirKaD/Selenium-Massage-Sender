bb53c901bc417e6721b13734de0395e5dddcc0f0
MeirKaD <92692385+MeirKaD@users.noreply.github.com>
2023-08-27 10:47:22 +0300

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import phonenumbers
-phonenum="528728774"
-parsed_number = phonenumbers.parse(phonenum, "IL")  # Assuming numbers are from Israel (IL)
-phone_number = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
-print (phone_number)
